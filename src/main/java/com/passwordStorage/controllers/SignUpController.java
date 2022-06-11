d28ef5b498dca7f8b9ba5d027f7d954fad9afc9b
MxtthwGucci <matveygucci@gmail.com>
2022-06-12 00:20:48 +0300

    Added password correct format check

--- src/main/java/com/passwordStorage/controllers/SignUpController.java
+++ src/main/java/com/passwordStorage/controllers/SignUpController.java
@@ -2,13 +2,17 @@ package com.passwordStorage.controllers;
 
 import com.passwordStorage.models.Storage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.passwordStorage.repo.PostRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -16,10 +20,12 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.Locale;
 
 @Controller
 public class SignUpController {
     String salt;
+
     public PostRepository getPostRepository() {
         return postRepository;
     }
@@ -27,26 +33,74 @@ public class SignUpController {
     public void setPostRepository(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
+
     @GetMapping("/signup")
-    public String getMapping(Model model)
-    {
+    public String getMapping(Model model) {
         return "signup";
     }
+
     @Autowired
     private PostRepository postRepository;
-    @PostMapping("/signup")
-    public String signUpMain(Model model,@RequestParam("signLogin") String login, @RequestParam("signPassword") String password, @RequestParam("signName") String name, @RequestParam("signSecondName") String secondName, @RequestParam("signMail") String mail) throws NoSuchAlgorithmException {
 
-        byte[]salt = saltMaker();
+    @PostMapping("/signup")
+    public String signUpMain(ModelMap model, @RequestParam("signLogin") String login, @RequestParam("signPassword") String password, @RequestParam("signName") String name, @RequestParam("signSecondName") String secondName, @RequestParam("signMail") String mail) throws NoSuchAlgorithmException {
+        if (!passChecker(password).equals(""))
+        {
+            model.addAttribute("error",passChecker(password));
+            return "signup";
+        }
+
+        byte[] salt = saltMaker();
         String hashedPassword = hashPass(password, salt);
         String salty = Base64.getEncoder().encodeToString(salt);
-        Storage storage = new Storage(login,hashedPassword,name,secondName,mail,salty);
+        Storage storage = new Storage(login, hashedPassword, name, secondName, mail, salty);
 
         postRepository.save(storage);
         return "login";
 
     }
-    public String hashPass (String password, byte[] salty) throws NoSuchAlgorithmException {
+
+    public String passChecker(String password) {
+        String nums = "0123456789";
+        String symbols = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+        StringBuilder checkResult = new StringBuilder("");
+        boolean lowCase = false;
+        boolean upCase = false;
+        boolean withoutNum = false;
+        boolean withoutSymbols = false;
+        if (password.length() <= 12) {
+            checkResult.append("Please increase password length");
+        }
+        if (password.toLowerCase(Locale.ROOT).equals(password)) {
+            lowCase = true;
+            checkResult.append("Please add capital letters"+"\n");
+        }
+        for (int i = 0; i < nums.length(); i++) {
+            if (password.contains(String.valueOf(nums.charAt(i)))) {
+                withoutNum = true;
+            }
+        }
+        if (!withoutNum)
+        {
+            checkResult.append("Please add numbers"+"\n");
+        }
+        if (password.toUpperCase(Locale.ROOT).equals(password)) {
+            upCase = true;
+            checkResult.append("Please add small letters"+"\n");
+        }
+        for (int i = 0; i < symbols.length(); i++) {
+            if (password.contains(String.valueOf(symbols.charAt(i)))) {
+                withoutSymbols = true;
+
+            }
+        }
+        if (!withoutSymbols) {
+            checkResult.append("Please add symbols"+"\n");
+        }
+        return checkResult.toString();
+    }
+
+    public String hashPass(String password, byte[] salty) throws NoSuchAlgorithmException {
         StringBuilder hashPass = new StringBuilder();
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         md.update(salty);
@@ -57,8 +111,8 @@ public class SignUpController {
         }
         return hashPass.toString();
     }
-    public byte[] saltMaker()
-    {
+
+    public byte[] saltMaker() {
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
